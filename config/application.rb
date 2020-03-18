73e1fdf10b856bc5b0af0296ed10d7d5de2b7519
Yuya Fujiwara <asonas@cookpad.com>
2020-03-18 20:04:35 +0900

    skip some generator

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Rails6base
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+      g.model migration: false
+      g.skip_routes true
+      g.test_framework :rspec, view_specs: false, helper_specs: false
+    end
   end
 end
